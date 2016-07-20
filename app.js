8179c756a2b91907b1b512d407f216e9997b7d56
camdeardorff <cameron.deardorff@yahoo.com>
2016-07-20 01:23:44 -0400

    Fixed hard coded port to listen on

--- app.js
+++ app.js
@@ -109,7 +109,7 @@ app.get('/', function (req, res) {
 // });
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
 	var host = 'localhost';
 	var port = server.address().port;
 	console.log('App listening at http://%s:%s', host, port);
