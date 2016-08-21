52ca8f0f58a94c207db373f66a0b89419de6d358
camdeardorff <cameron.deardorff@yahoo.com>
2016-08-21 11:24:01 -0400

    Refactoring illness model, removing warnings.

--- models/illness.js
+++ models/illness.js
@@ -1,14 +1,14 @@
 /*
-	FILE: illness.js (model)
-	PURPOSE: to model an illness and interact with the database by providing a set of functions to interact with the database
-	FUNCTIONS: 
-		- constructor
-		- get data
-		- sanitize data
-		- save
-		- get all
-		- get by id
-*/
+ FILE: illness.js (model)
+ PURPOSE: to model an illness and interact with the database by providing a set of functions to interact with the database
+ FUNCTIONS:
+ - constructor
+ - get data
+ - sanitize data
+ - save
+ - get all
+ - get by id
+ */
 
 
 //import necessary dependancies
@@ -20,44 +20,46 @@ var _ = require("lodash");
 function Illness(data) {
 	//sanitize the data and store it away
 	this.data = this.sanitize(data);
-};
+}
 
 //illness data access function
 Illness.prototype.getData = function () {
 	return this.data;
 };
 
-//sanitize data with use of illness schema and lodash
+//sanitize data with use of illness schema and lo dash
 Illness.prototype.sanitize = function (data) {
 	//use provided data or use an empty obj (NO NULL)
 	data = data || {};
 	//use the illness schema
-	schema = schemas.illness;
+	var schema = schemas.illness;
 	return _.pick(_.defaults(data, schema), _.keys(schema));
 };
 
 /*
-	FUNCTION: save
-	PURPOSE: create a new entry in the illness table of the database. Uses data that has been sanitized using the illness schema.
-	PARAMS: callback: signals the caller to handle the reprocussions but most importantly returns values.
-*/
+ FUNCTION: save
+ PURPOSE: create a new entry in the illness table of the database. Uses data that has been sanitized using the illness schema.
+ PARAMS: callback: signals the caller to handle the repercussion but most importantly returns values.
+ */
 //TODO: save only new/unique illnesses
 Illness.prototype.save = function (callback) {
 	//make an obj of this illness for forward reference.
-	ill = this;
-	//two arrays. one for the columns of the query and another for thier new values
-	var cols = [];
-	var vals = [];
+	var holdIllness = this;
+	//two arrays. one for the columns of the query and another for their new values
+	var columns = [];
+	var values = [];
 	//loop over every data point in the illness data
-	for (var col in this.data) {
+	for (var column in this.data) {
 		//push into their respective arrays
-		cols.push(col);
-		vals.push(this.data[col])
+		if (this.data.hasOwnProperty(column)) {
+			columns.push(column);
+			values.push(this.data[column])
+		}
 	}
 
 	//create a new entry in the database for this illness
 	db.getConnection().query("INSERT INTO `illness` (??) " +
-		"VALUES (?);", [cols, vals],
+		"VALUES (?);", [columns, values],
 		function (err, result) {
 			//hand an error if there is one
 			if (err) {
@@ -65,12 +67,11 @@ Illness.prototype.save = function (callback) {
 				callback(err);
 			} else {
 				//no error. update this illness' id with the insert id (auto increment)
-				ill.data.id = result.insertId;
+				holdIllness.data.id = result.insertId;
 				callback(null, result.insertId);
 			}
 		});
 };
-
 
 
 Illness.prototype.update = function (callback) {
@@ -84,24 +85,23 @@ Illness.prototype.update = function (callback) {
 			var values = [];
 			var inserts = "";
 			var insert = "?? = ?";
-			for (var val in illnessData) {
-				console.log("loopty loop");
-				console.log(illnessData[val]);
-				values.push(val);
-				values.push(illnessData[val]);
-				inserts += insert + ",";
+			for (var value in illnessData) {
+				if (illnessData.hasOwnProperty(value)) {
+					console.log(illnessData[value]);
+					values.push(value);
+					values.push(illnessData[value]);
+					inserts += insert + ",";
+				}
 			}
 			values.push(illnessData.id);
-			inserts = inserts.slice(0,-1);
+			inserts = inserts.slice(0, -1);
 
 //TODO
 
 
-
-
 			console.log(inserts);
 			var query = "UPDATE `illness` SET " + inserts + " WHERE `illness`.`id` =  ?;";
-			var q = db.getConnection().query(query, values, function (err, result) {
+			db.getConnection().query(query, values, function (err, result) {
 				if (err) {
 					console.log(err);
 					callback(err);
@@ -123,8 +123,8 @@ Illness.prototype.delete = function (callback) {
 			callback(err || "No such illness with id: " + illnessData.id);
 		} else {
 			//the illness exists. lets delete it
-			var query = "DELETE FROM `illness` WHERE `illness`.`id` = ?"
-				var q = db.getConnection().query(query, illnessData.id, function (err, result) {
+			var query = "DELETE FROM `illness` WHERE `illness`.`id` = ?";
+			db.getConnection().query(query, illnessData.id, function (err, result) {
 				if (err) {
 					console.log(err);
 					callback(err);
@@ -139,16 +139,14 @@ Illness.prototype.delete = function (callback) {
 };
 
 
-
-
 /*
-	FUNCTION: get all
-	PURPOSE: to retrieve all of the illnesses currently in the database
-	PARAMS: callback: signals the caller to handle the reprocussions but most importantly returns values.
-*/
+ FUNCTION: get all
+ PURPOSE: to retrieve all of the illnesses currently in the database
+ PARAMS: callback: signals the caller to handle the repercussions but most importantly returns values.
+ */
 Illness.getAll = function (callback) {
 	//query the database for all illnesses
-	db.getConnection().query("SELECT * FROM `illness`;", function (err, rows, fields) {
+	db.getConnection().query("SELECT * FROM `illness`;", function (err, rows) {
 		//check for an error and return one if there is
 		if (err) {
 			callback(err);
@@ -163,21 +161,21 @@ Illness.getAll = function (callback) {
 			callback(null, illnesses);
 		}
 	});
-}
+};
 
 
 /*
-	FUNCTION: get by id
-	PURPOSE: gets an illness that corresponds to the id provided
-	PARAMS: id: the id of the illness to be returned
-			callback: signals the caller to handle the reprocussions but most importantly returns values.
-*/
+ FUNCTION: get by id
+ PURPOSE: gets an illness that corresponds to the id provided
+ PARAMS: id: the id of the illness to be returned
+ callback: signals the caller to handle the reprocussions but most importantly returns values.
+ */
 
 Illness.getById = function (id, callback) {
 	//query the database for the illness that relates to the id provided
 	db.getConnection().query("SELECT * FROM `illness` " +
 		"WHERE `id` = ?", [id],
-		function (err, rows, fields, result) {
+		function (err, rows) {
 			//check for an error
 			if (err) {
 				//log it out
@@ -187,7 +185,7 @@ Illness.getById = function (id, callback) {
 				//no error but there may not have been a matching illness
 				if (rows.length > 0) {
 					//there was an illness that matched the id. make it into an illness and send it back
-					illness = new Illness(rows[0]);
+					var illness = new Illness(rows[0]);
 					callback(null, illness);
 				} else {
 					//there was no matching illness
@@ -195,10 +193,7 @@ Illness.getById = function (id, callback) {
 				}
 			}
 		});
-}
-
-
-
+};
 
 
 module.exports = Illness;
